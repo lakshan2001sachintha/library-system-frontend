bd511540e1982f323eda421d4d7909e10ffb0840
Sachintha_Lakshan <sachintha2001lakshan@gmail.com>
2025-11-03 16:49:26 +0530

    initialize the frontend application

---
+++ LibraryFrontend/src/App.tsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-gray-100">
+      <h1 className="text-3xl font-bold text-blue-600">
+        Hello Tailwind 3.4.12 + React + TS!
+      </h1>
+    </div>
+  )
+}
